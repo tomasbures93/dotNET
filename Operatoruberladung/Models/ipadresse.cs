a974e8b59d0e8ba79398956addd9a12affc4c340
BurciWolf <98662129+BurcisWolf@users.noreply.github.com>
2025-02-24 15:41:29 +0100

    IP adressen operatoren

---
+++ Operatoruberladung/Models/ipadresse.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatoruberladung.Models
+{
+    internal class ipadresse
+    {
+        private ulong _erstOktet;
+        private ulong _zweiteOktet;
+        private ulong _dritteOktet;
+        private ulong _vierteOktet;
+
+        public ipadresse(ulong erst, ulong zweitt, ulong dritte, ulong vierte)
+        {
+            _erstOktet = erst;
+            _zweiteOktet = zweitt;
+            _dritteOktet = dritte;
+            _vierteOktet = vierte;
+        }
+
+        public override string ToString()
+        {
+            return $"{_erstOktet}.{_zweiteOktet}.{_dritteOktet}.{_vierteOktet}";
+        }
+
+        public static ipadresse operator ++(ipadresse adresse)
+        {
+            ulong ein = adresse._erstOktet;
+            ulong zwei = adresse._zweiteOktet;
+            ulong drei = adresse._dritteOktet;
+            ulong vier = adresse._vierteOktet;
+            bool nachsteOktet = true;
+            if(vier >= 255)
+            {
+                vier = 0;
+                drei++;
+                nachsteOktet = false;
+            } 
+            else
+            {
+                vier++;
+                return new ipadresse(ein, zwei, drei, vier);
+            }
+            if(drei >= 255)
+            {
+                drei = 0;
+                zwei++;
+                nachsteOktet = false;
+            } 
+            else if(nachsteOktet == true)
+            {
+                drei++;
+                return new ipadresse(ein, zwei, drei, vier);
+            }
+            if (zwei >= 255)
+            {
+                zwei = 0;
+                if(ein < 255)
+                {
+                    ein++;
+                }
+                nachsteOktet = false;
+            } 
+            else if(nachsteOktet == true)
+            {
+                zwei++;
+                return new ipadresse(ein, zwei, drei, vier);
+            }
+            if(ein >= 255)
+            {
+                Console.WriteLine("NOPE, adresse 256.0.0.0 existiert nicht");
+                ein = 256;
+                zwei = 256;
+                drei = 256;
+                vier = 256;
+            } 
+            else if(nachsteOktet == true)
+            {
+                ein++;
+            }
+            return new ipadresse(ein, zwei, drei, vier);
+        }
+    }
+}
---
+++ Operatoruberladung/Program.cs
@@ -0,0 +1,62 @@
+﻿using Operatoruberladung.Models;
+
+namespace Operatoruberladung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ipadresse deffault = new ipadresse(192, 168, 2, 253);
+            //Console.WriteLine(deffault.ToString());
+            //deffault++;
+            //Console.WriteLine(deffault.ToString());
+            //Console.WriteLine("");
+            //ipadresse deffault2 = new ipadresse(192, 168, 2, 254);
+            //Console.WriteLine(deffault2.ToString());
+            //deffault2++;
+            //Console.WriteLine(deffault2.ToString());
+
+            //Console.WriteLine("");
+            //ipadresse deffault3 = new ipadresse(192, 168, 2, 255);
+            //Console.WriteLine(deffault3.ToString());
+            //deffault3++;
+            //Console.WriteLine(deffault3.ToString());
+            //deffault3++;
+            //Console.WriteLine(deffault3.ToString());
+
+            //Console.WriteLine("");
+            //ipadresse deffault4 = new ipadresse(192, 168, 255, 255);
+            //Console.WriteLine(deffault4.ToString());
+            //deffault4++;
+            //Console.WriteLine(deffault4.ToString());
+            //deffault4++;
+            //Console.WriteLine(deffault4.ToString());
+            //deffault4++;
+            //Console.WriteLine(deffault4.ToString());
+
+            //Console.WriteLine("");
+            //ipadresse deffault5 = new ipadresse(192, 255, 255, 255);
+            //Console.WriteLine(deffault5.ToString());
+            //deffault5++;
+            //Console.WriteLine(deffault5.ToString());
+            //deffault5++;
+            //Console.WriteLine(deffault5.ToString());
+            //deffault5++;
+            //Console.WriteLine(deffault5.ToString());
+
+            //Console.WriteLine("");
+            //ipadresse deffault6 = new ipadresse(254, 255, 255, 255);
+            //Console.WriteLine(deffault6.ToString());
+            //deffault6++;
+            //Console.WriteLine(deffault6.ToString());
+            //deffault6++;
+            //Console.WriteLine(deffault6.ToString());
+
+            Console.WriteLine("");
+            ipadresse deffault7 = new ipadresse(255, 255, 255, 255);
+            Console.WriteLine(deffault7.ToString());
+            deffault7++;
+            Console.WriteLine(deffault7.ToString());
+        }
+    }
+}
