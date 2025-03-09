67ba513b8e1104cb0f1c47fc17664e2a7abb967d
BurcisWolf <furcis.burcis@gmail.com>
2025-03-09 01:10:56 +0100

    test

---
+++ Database - SQLite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Database___SQLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
